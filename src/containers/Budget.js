cc2f0db0f4b3b1751ba1cdd7daae081c259fc9f4
lemarti <martinoleandr@gmail.com>
2023-03-13 19:19:57 -0300

    firebase

---
+++ src/containers/Budget.js
@@ -0,0 +1,4 @@
+export default function Budget() {
+    return <div><h1>Calcula tu presupuesto!</h1>
+       </div>
+}
---
+++ src/pages/about.js
@@ -0,0 +1,5 @@
+import Link from 'next/link'
+export default function About() {
+    return <div><h1>About page</h1>
+        <Link href='/'>Volver a inicio</Link> </div>
+}
